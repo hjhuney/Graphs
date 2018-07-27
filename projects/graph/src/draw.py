96a1a767ce5d05f8bdb3b0e24f42b5168524d9b0
Jake Huneycutt <hjhuney@gmail.com>
2018-07-27 13:06:46 -0400

    add files

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -10,5 +10,86 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph, title='Graph', width=10, height=10, show_axis=False, show_grid=False, circle_size=35, draw_components=False):
+        if not graph.vertices:
+            raise Exception('graph should contain vertices!')
+        self.graph = graph
+        self.width = width
+        self.height = height
+        self.pos = {}
+        # set up plot, canvas space
+        self.plot = figure(title=title, x_range=(0, width), y_range=(0, height)
+        self.plot.axis.visible = show_axis
+        self.plot.grid.visible = show_grid
+        self._setup_graph_renderer(circle_size, draw_components)
+        self._setup_labels()
+
+    def _setup_graph_renderer(self, circle_size, draw_components=False):
+        # render will have actual logic for drawing
+        graph_renderer = GraphRenderer()
+        # saving vertices in arbitrar but persistent order
+        self.vertex_keys = list(self.graph.vertices.keys())
+
+        # add vertex data as instructions for drawing nodes
+        graph_renderer.node_renderer.data_source.add(self.vertex_keys, 'index')
+        if draw_components:
+            # components will be random colors
+            graph_renderer.node_renderer_data_source.add(
+                self._get_connected_component_colors(), 'color')
+        else: 
+            # nodes will be random colors
+            graph_renderer.node_renderer.data_source.add(
+                self._get_random_colors(), 'color')
+
+    def _get_random_colors(self, num_colors=None):
+        colors =[]
+        num_colors = num_colors or (len(self.graph.vertices))
+        for _ in range(num_colors):
+            color = '#' + ''.join([choice('0123456789ABCDEF') for j in range(6)])
+            colors.append(color)
+        return colors
+
+    def _get_edge_indexes(self):
+        start_indices = []
+        end_indices = []
+        checked = set()
+
+        for vertex, edges in self.graph.vertices.items():
+            if vertex not in checked:
+                for destination in edges:
+                    start_indices.append(vertex)
+                    end_indices.append(destination)
+                checked.add(vertex)
+
+        return dict(start=start_indices, end=end_indices)
+
+
+    def _setup_labels(self):
+        label_data = {'x': [], 'y': [], 'names': []}
+        for vertex, position in self.pos.items():
+            label_data['x'].append(position[0])
+            label_data['y'].append(position[1])
+            label_data['names'].append(vertex.label)
+        label_source = ColumnDataSource(label_data)
+        labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center', text_baseline='middle', source=label_source, render_mode='canvas')
+        self.plot.add_layout(labels)
+
+    def show(self.output_path='./graph.html'):
+        output_file(output_path)
+        show(self.plot)
+
+    def randomize(self):
+        """ randomize vertex positions """
+        for vertex in self.graph.vertices:
+            # TODO make boundds and random draws less hacky
+            self.pos[vertex] = (1+ random() * (self.width -2), 
+            1 + random() * (self.height -2))
+
+    def _color_connected_components(self):
+        """ return same colors for vertices in connected components """
+        self.graph.find_components()
+        component_colors = self._get_random_colors(self.graph.components)
+        vertex_colors = []
+        for vetex in self.vertex_keys:
+            vertex_colors.append(component_colors[vertex.component])
+        return vertex_colors
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,8 +4,93 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+from collections import namedtuple
+Vertex = namedtuple('Vertex', ['label', 'component'])
+
+
+class Vertex:
+    """ Represent a vertex with a label and possible connected component"""
+    def __init__(self, label, component=-1):
+        self.label = label
+        self.component = component
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+      self.vertices = {}
+      
+    def add_vertex(self,vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices"""
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertext that already exists')
+        self.vertices[vertex] = set(edges)
+        
+    def add_edge(self, start, end, bidirectional=True):
+        """ Add a edge (default bidirectional) between two vertices """
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
+
+    # def bfs(self, start, target=None):
+    #     queue = [start]
+    #     visited = set()
+
+    #     while queue:
+    #         current = queue.pop(0)
+    #         if current == target:
+    #             break
+    #         visited.add(current)
+    #         # add possible (unvisited) vertices to queue
+    #         queue.extend(self.vertices[current] - visited)
+
+    #     return visited
+
+    # def dfs(self, start, target=None):
+    #     stack = [start]
+    #     visited = set()
+
+    #     while stack:
+    #         current = stack.pop(0)
+    #         if current == target:
+    #             break
+    #         visited.add(current)
+    #         # add possible (unvisited) vertices to queue
+    #         stack.extend(self.vertices[current] - visited)
+
+    #     return visited
+
+    def search(self, start, target=None, method='dfs'):
+        """ search the graph using BFS or DFS. """
+        quack = [start]    # queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
+
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            # add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
+
+        return visited
+
+    def find_components(self):
+        """ Identify components and update vertex component IDs. """
+        visited = set()
+        current_component = 0
+
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
